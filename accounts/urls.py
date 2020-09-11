a5ad6b5981bcd411ae81d3eeb627c5b9ecffb51d
jangwonie <s2018w17@e-mirim.hs.kr>
2020-09-11 15:21:05 +0900

    templates/accounts login, logout, base

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name = 'login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name = 'logout')
+    path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name = 'logout'),
 ]
